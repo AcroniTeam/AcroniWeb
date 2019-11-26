5ab29e170b80d8f75c5de5acc897ba8ce03e0ebd
Gigademolidor <39094702+Gigademolidor@users.noreply.github.com>
2019-11-26 18:21:33 -0300

    Correção erros ASP trincando pra amanhã

--- AcroniWeb/AcroniWeb-4.5/View/loja.aspx.cs
+++ AcroniWeb/AcroniWeb-4.5/View/loja.aspx.cs
@@ -18,7 +18,13 @@ namespace AcroniWeb
         [System.Web.Services.WebMethod]
         public static double calcularFrete(string cep, string id)
         {
-            List<string> teclado = sql.selectCampos("*", "tblProdutoDaLoja", "id_produto = " + id + "");
+            List<String> teclado;
+            if (HttpContext.Current.Session["custom"] == null)
+                teclado = sql.selectCampos("*", "tblProdutoDaLoja", "id_produto = " + id + "");
+            else
+                teclado = sql.selectCampos("*", "tblProdutoDaLoja", "id_produto = 2");
+
+
             var correios = new AcroniWeb_4._5.WSCorreiosFrete.CalcPrecoPrazoWSSoapClient();
             var resposta = correios.CalcPrecoPrazo(string.Empty, string.Empty, "40010", "01101010", cep, teclado[3].ToString(), 1, decimal.Parse(teclado[6]), decimal.Parse(teclado[4]), decimal.Parse(teclado[5]), 0, "N", decimal.Parse(teclado[7]), "N");
             var respostaReal = resposta.Servicos.FirstOrDefault();
